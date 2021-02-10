dd0130761fdc1ec71de94b25c5250b85a9a50ae1
Goodwin <bogdan411@rmabler.ru>
2021-02-10 17:27:23 +0200

    JWT validation func. updated (Bearer-Token)

--- middleware/validate.token.js
+++ middleware/validate.token.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken'
 require('dotenv').config()
 
+
 function verifyToken (req, res, next) {
-    const token = req.header('auth-token')
-    if (!token) return res.status(401).json({ error: 'Access denied' })
+    const token = req.header('Authorization').split(' ')
+    if (token[0] !== 'Bearer') return res.status(401).json({ error: 'Access denied' })
 
     try {
-        const verified = jwt.verify(token, process.env.SECRET)
+        const verified = jwt.verify(token[1], process.env.SECRET)
         req.user = verified
         next()
     } catch(e) {
@@ -15,3 +16,22 @@ function verifyToken (req, res, next) {
 }
 
 export default verifyToken
+
+
+
+
+
+// function verifyToken (req, res, next) {
+//     const token = req.header('Authorization')
+//     if (!token) return res.status(401).json({ error: 'Access denied' })
+
+//     try {
+//         const verified = jwt.verify(token, process.env.SECRET)
+//         req.user = verified
+//         next()
+//     } catch(e) {
+//         res.status(400).json({ error: 'Token is not valid' })
+//     }
+// }
+
+// export default verifyToken
